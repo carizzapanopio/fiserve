ede36d7e200934d68b1ee7cb6967e4856a6f5539
carizza panopio <carizzapanopio@gmail.com>
2018-07-30 19:50:04 +0800

    update on validation

--- app/Http/Requests/ConvertRequest.php
+++ app/Http/Requests/ConvertRequest.php
@@ -26,8 +26,8 @@ class ConvertRequest extends FormRequest
     public function rules()
     {
         return [
-            'params.amount'       =>  array('required','regex:/^\d*(\.\d{1,2})?$/'), //amount
-            'params.currency'     => 'required', //currency
+            'params.amount'       =>  array('required', 'gt:0', 'regex:/^\d*(\.\d{1,2})?$/'), //amount
+            'params.currency'     => 'required|exists:currencies,code', //currency
             'params.published_at' => 'required|date|exists:rates,published_at,currency,'.$this->input('params.currency'), //published at
         ];
     }
@@ -40,9 +40,18 @@ class ConvertRequest extends FormRequest
     public function messages()
     {
         return [
-            'regex'  => 'The :attribute should only have a maximum of 2 decimal places',
-            'date'   => 'The specified date is not in the correct format.',
-            'exists' => 'No rates found on the requested date.',
+            'params.amount.required'       => "The <b>amount</b> field is required.",
+            'params.currency.required'     => "The <b>currency</b> field is required.",
+            'params.published_at.required' => "The <b>date</b> field is required.",
+
+            'params.amount.required'       => "The <b>amount</b> field should be greater than 0.",
+            'params.amount.regex'          => 'The <b>amount</b> should only have a maximum of 2 decimal places',
+
+            'params.currency.exists' => "The <b>code</b> does not exist.",
+
+            'params.published_at.date'   => 'The specified <b>date</b> is not in the correct format.',
+            'params.published_at.exists' => 'No rates found on the requested date and currency.',
+
         ];
     }
 
